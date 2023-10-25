1edb03eed68952c2112d351a57a7612f84d3ea2c
AriqHakim <ariq.hakim2002@gmail.com>
2023-10-25 11:35:29 +0700

    add endpoint for remove account

--- backend/src/modules/profile/Profile.route.ts
+++ backend/src/modules/profile/Profile.route.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { getMyProfile } from './presentation/GetMyProfile.presentation';
 import { getProfileById } from './presentation/GetProfileById.presentation';
+import { removeAccount } from './presentation/RemoveAccount.presentation';
 
 const router = express.Router();
 
 router.get('/profile', getMyProfile);
 router.get('/profile/:id', getProfileById);
+router.delete('/profile/remove', removeAccount);
 
 export default router;
