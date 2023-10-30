8aa269c48d7ace4f432991145891b7f31e8740dd
AriqHakim <ariq.hakim2002@gmail.com>
2023-10-30 17:56:56 +0700

    add endpoint for share location

--- backend/src/modules/location/Location.route.ts
+++ backend/src/modules/location/Location.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { GetFriendLocation } from './presentation/GetFriendLocation.presentation';
+import { shareLocation } from './presentation/ShareLocation.presentation';
 
 const router = express.Router();
 
 router.get('/locations', GetFriendLocation);
+router.put('/locations', shareLocation);
 
 export default router;
